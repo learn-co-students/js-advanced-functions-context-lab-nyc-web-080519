21740b7ecebb65425eb4b421036e9a48fc8e316f
github username <github email address>
2019-09-26 13:51:43 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,59 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function (arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = employeeData => {
+    return employeeData.map(arr => createEmployeeRecord(arr))
+}
+
+let createTimeInEvent = function(dateTime) {
+    const [date, hour] = dateTime.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour), 
+        date
+    })
+    return this
+}
+
+
+let createTimeOutEvent = function(dateTime){
+    const [date, hour] = dateTime.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date 
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(date){
+    let inEvent = this.timeInEvents.find(timeStamp => timeStamp.date == date
+    )
+
+    let outEvent = this.timeOutEvents.find(timeStamp => timeStamp.date === date)
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date) {
+    let wagesEarned = hoursWorkedOnDate.call(this, date) * this.payPerHour //needs the .call in order to bind that current this to hoursWorkedOnDate
+    return wagesEarned
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +72,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let findEmployeebyFirstName = function(collection, firstNameString){
+    return collection.find(employee => employee.firstName === firstNameString) 
+}
+
+let calculatePayroll = arrayOfEmployeeRecords => {
+    return arrayOfEmployeeRecords.reduce((acc, rec) => acc + allWagesFor.call(rec)
+        , 0)
 }
