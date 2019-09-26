8446b1b12dad0f164809b5679b50042817722aa2
Mason Balbera <mhbalbera@gmail.com>
2019-09-26 13:51:24 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,85 @@
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
+ */
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+let createEmployees = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(dte){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dte
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dte
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dte){
+    let w = hoursWorkedOnDate.call(this, dte)
+        * this.payPerHour
+    return parseFloat(w.toString())
+}
+
+
+let createEmployeeRecords = function(src) {
+  return src.map(function(row){
+    return createEmployeeRecord(row)
+  })
+}
+
+let findEmployeebyFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+/*
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
@@ -20,3 +98,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
